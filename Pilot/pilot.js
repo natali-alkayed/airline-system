e91ebe70f84fe6888293117b178c66de823e281d
natali-alkayed <nataly2000alkayed@gmail.com>
2023-07-10 10:14:36 +0000

    lab 13 done

--- Pilot/pilot.js
+++ Pilot/pilot.js
@@ -7,18 +7,40 @@ const AirLineConnection = io.connect(host);
 let host2 = `http://localhost:${port}/`;
 const SystemConnection = io.connect(host2);
 
+SystemConnection.emit('get-all');
 
- 
+
   AirLineConnection.on('took-off-log', (flightId) => {
         console.log(`Pilot: Flight with ID '${flightId}' took off.`);
         console.log("-------------------------------------");})
    
 
-    
     AirLineConnection.on('arrived-log', (flightId) => {
         console.log(`Pilot: Flight with ID '${flightId}' arrived.`);
-        console.log("-------------------------------------");})
-      
+        console.log("-------------------------------------");
+        SystemConnection.emit('flight-arrived', flightId);
+        SystemConnection.emit('delete', (flightId));
+      })
+///////////////////////////////////////////////////////////////////////////
+
+SystemConnection.on('flight', (flights) => {
+  
+  if (flights.length === 0) {
+    console.log("Pilot: Sorry, I didn't catch any flight IDs.");
+  
+  } else {
+    console.log("Pilot: Sorry, I didn't catch these flight IDs:", flights.map(flight => flight.flightId).join(', '));
+  }
+
+  if (flights.flightId !== '332u443673r32yuf463') {
+    setInterval(() => {
+      console.log("Pilot: Sorry, I didn't catch this flight ID:332u443673r32yuf463");
+    }, 200000); 
+  }
+  
+});
+
+
 
 
 
--- System/system.js
+++ System/system.js
@@ -5,14 +5,21 @@ const socket = require('socket.io');
 const ioServer = socket(port);
 const AirLineConnection = ioServer.of('/airline');
 const moment = require('moment');
+const uuid = require('uuid').v4;
 
+const queue = {
+  flights: {
+
+  }
+};
 
 ioServer.on('connection', (socket) => {
   console.log('connected ', socket.id);
 
-function printFlightDetails(event, flightDetails) {
+  function printFlightDetails(event, flightDetails) {
     const formattedTime = moment().format('YYYY-MM-DD HH:mm:ss');
     console.log(`Flight {
+      id: '${id},
       event: '${event}',
       time: ${formattedTime},
       Details: {
@@ -23,26 +30,44 @@ function printFlightDetails(event, flightDetails) {
       }
     }`);
   }
-
+  const id = uuid();
   socket.on('new-flight', (flightDetails) => {
-  printFlightDetails('new-flight', flightDetails);
-  socket.emit('new-flight', flightDetails);
+   // const id = uuid();
+    flightDetails.id = id;
+    queue.flights[id] = flightDetails;
+    printFlightDetails('new-flight', flightDetails);
+    socket.emit('new-flight', flightDetails);
 
-  setTimeout(() => {
-    AirLineConnection.emit('took-off', flightDetails);
-    printFlightDetails('took_off', flightDetails);
-    AirLineConnection.emit('took-off-log', flightDetails.flightId);
-  }, 4000);
- 
-  setTimeout(() => {
-    socket.emit('Arrived', flightDetails);
-    printFlightDetails('Arrived', flightDetails);
-    AirLineConnection.emit('arrived-log', flightDetails.flightId);
-  }, 7000);
- 
-});
+    setTimeout(() => {
+      AirLineConnection.emit('took-off', flightDetails);
+      printFlightDetails('took_off', flightDetails);
+      AirLineConnection.emit('took-off-log', flightDetails.flightId);
+    }, 4000);
+
+    setTimeout(() => {
+      socket.emit('Arrived', flightDetails);
+      printFlightDetails('Arrived', flightDetails);
+      AirLineConnection.emit('arrived-log', flightDetails.flightId);
+    }, 7000);
+
+  });
+  socket.on('get-all', () => {
+    const flights = Object.values(queue.flights);
+    socket.emit('flight', flights);
+    queue.flights = {};
+    console.log("*****************************",queue.flights);
+  });
+  
+  socket.on('delete', (id) => {
+    delete queue.flights[id];
+    console.log("queue v2 after arrived and delete specific id ",queue);
+  });
 
 })
+
+
+
+
 
 
 
